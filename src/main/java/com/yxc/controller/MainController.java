9317de396e07bfa4091c5646b563999caebc3988
杨晓晨 <xiaochenyung@gmail.com>
2016-10-12 14:19:47 +0800

    find

--- src/main/java/com/yxc/controller/MainController.java
+++ src/main/java/com/yxc/controller/MainController.java
@@ -40,6 +40,7 @@ public class MainController {
     public String getUsers(ModelMap modelMap){
 
         List<UserEntity> userList = userRepository.findAll();
+//        List<UserEntity> userList = userRepository.findAll()
         modelMap.addAttribute("userList",userList);
         return "admin/users";
     }
